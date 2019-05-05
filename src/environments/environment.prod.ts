3589911cbfc3a383f106bedc193fa8e34f81bfeb
Ahmed Al-naggar <alnaggar98@hotmail.com>
2019-05-05 16:10:49 +0200

    the first section is done successfully !

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAJzL34eSMmT37Mt7HbewasRKkruiOp8gM",
+    authDomain: "oshop-852de.firebaseapp.com",
+    databaseURL: "https://oshop-852de.firebaseio.com",
+    projectId: "oshop-852de",
+    storageBucket: "oshop-852de.appspot.com",
+    messagingSenderId: "235543834972",
+    appId: "1:235543834972:web:95262eff2e4b9414"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAJzL34eSMmT37Mt7HbewasRKkruiOp8gM",
+    authDomain: "oshop-852de.firebaseapp.com",
+    databaseURL: "https://oshop-852de.firebaseio.com",
+    projectId: "oshop-852de",
+    storageBucket: "oshop-852de.appspot.com",
+    messagingSenderId: "235543834972",
+    appId: "1:235543834972:web:95262eff2e4b9414"
+  }
 };
 
 /*
